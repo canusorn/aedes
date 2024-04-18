eb428d8a14a01430ef65518a879b40202f0bfb87
canusorn <39732595+canusorn@users.noreply.github.com>
2024-04-18 21:37:32 +0700

    vue

---
+++ examples/clusters/vue/chart/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <BarChart />
+</template>
+
+<script>
+import BarChart from './components/BarChart'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+</script>
---
+++ examples/clusters/vue/chart/src/components/BarChart.vue
@@ -0,0 +1,40 @@
+<template>
+    <Line id="my-chart-id" :options="chartOptions" :data="chartData" />
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+import {
+    Chart as ChartJS, CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend)
+
+export default {
+    name: 'BarChart',
+    components: { Line },
+    data() {
+        return {
+            chartData: {
+                labels: ['January', 'February', 'March'],
+                datasets: [{ data: [40, 20, 12] }]
+            },
+            chartOptions: {
+                responsive: true
+            }
+        }
+    }
+}
+</script>
